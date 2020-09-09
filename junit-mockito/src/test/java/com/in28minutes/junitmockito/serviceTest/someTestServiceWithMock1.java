7f0b776a2378c5184faff0ad44079a812d794b31
monksmojo <makkarudit8@gmail.com>
2020-09-10 04:07:46 +0530

    completed till section 2 video 13

---
+++ junit-mockito/src/test/java/com/in28minutes/junitmockito/serviceTest/someTestServiceWithMock1.java
@@ -0,0 +1,49 @@
+package com.in28minutes.junitmockito.serviceTest;
+
+import com.in28minutes.junitmockito.data.SomeDataImpl;
+import com.in28minutes.junitmockito.service.SomeServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+// refined
+public class someTestServiceWithMock1 {
+
+
+    SomeServiceImpl someService=new SomeServiceImpl();
+
+    SomeDataImpl someDataMock=mock(SomeDataImpl.class);
+
+
+    @BeforeEach
+    public void beforeRunningEachTest(){
+
+        someService.setSomeData(someDataMock);
+    }
+
+    @Test
+    public void testingWithMockForNormalInput() {
+
+        when(someDataMock.retrieveData()).thenReturn(new int[] {1, 2, 3, 4});
+
+        assertEquals(10, someService.calculateSumWithDataService());
+    }
+
+    @Test
+    public void testingWithMockForNoInput(){
+        when(someDataMock.retrieveData()).thenReturn(new int[]{});
+
+        assertEquals(0,someService.calculateSumWithDataService());
+    }
+
+    @Test
+    public  void testingWithMockForSingleInput(){
+        when(someDataMock.retrieveData()).thenReturn(new int[]{5});
+
+        assertEquals(5,someService.calculateSumWithDataService());
+    }
+}
