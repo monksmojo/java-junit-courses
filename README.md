676afc4f4a36208e67c51dce720cf90dab6f3115
udit-makkar-ttn <udit.makkar@tothenew.com>
2020-09-22 02:29:32 +0530

    pushing the examples

---
+++ README.md
@@ -0,0 +1,5 @@
+Spring Boot - Unit Testing and Mocking with Mockito and JUnit
+
+link- https://www.springboottutorial.com/spring-boot-unit-testing-and-mocking-with-mockito-and-junit
+
+This guide will help you create great unit tests with JUnit and Mockito for your Spring Boot Projects. We will use two difference approaches to write unit tests
---
+++ junitEx-2/src/main/java/com/in28Minutes/junitExmple1/JunitExmple1Application.java
@@ -0,0 +1,13 @@
+package com.in28Minutes.junitExmple1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunitExmple1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JunitExmple1Application.class, args);
+	}
+
+}
