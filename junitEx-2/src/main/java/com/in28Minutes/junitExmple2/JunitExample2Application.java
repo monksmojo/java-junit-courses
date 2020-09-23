dd16b0ec4391febddb08fe7303bb4502b19ab6c2
monksmojo <makkarudit8@gmail.com>
2020-09-24 04:17:57 +0530

    complete example 2

--- junitEx-2/src/main/java/com/in28Minutes/junitExmple2/JunitExample2Application.java
+++ junitEx-2/src/main/java/com/in28Minutes/junitExmple2/JunitExample2Application.java
@@ -1,13 +1,13 @@
-package com.in28Minutes.junitExmple1;
+package com.in28Minutes.junitExmple2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class JunitExmple1Application {
+public class JunitExample2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(JunitExmple1Application.class, args);
+		SpringApplication.run(JunitExample2Application.class, args);
 	}
 
 }
--- junitEx-2/src/test/java/com/in28Minutes/junitExmple2/JunitExample2ApplicationTests.java
+++ junitEx-2/src/test/java/com/in28Minutes/junitExmple2/JunitExample2ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.in28Minutes.junitExmple1;
+package com.in28Minutes.junitExmple2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class JunitExmple1ApplicationTests {
+class JunitExample2ApplicationTests {
 
 	@Test
 	void contextLoads() {
